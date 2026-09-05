7bfbe41c8b3aa395670c7ff9ffb0e3e88d599b18
remi <squirregang@gmail.com>
2026-09-05 18:46:55 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class Corner:
+    def __init__(self, identity, position):
+        self.identity = identity
+        self.position = position
+
+corner1 = Corner("WRG", "UFR")
+#.identiy and .position are used to access the attributes of the object
+#corner1.identity, corner1.position
+corner2 = Corner("WGO", "UFL")
+#corner2.identity, corner1.position
