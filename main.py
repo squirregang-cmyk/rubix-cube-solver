c3c8f729e4945edfb80bfe2446f49c5dc2fb4364
remi <squirregang@gmail.com>
2026-09-06 13:31:25 +0100

    changed previous attributes and like incoperated them into dictionaries inside of a new like overall class called cube haing both edges and corners, changed some of the logic behind it aswell

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
 class Corner:
-    def __init__(self, identity, position):
+    def __init__(self, identity, orientation = 0):
         self.identity = identity
-        self.position = position
+        self.orientation = orientation
 
-corner1 = Corner("WRG", "UFR")
-#.identiy and .position are used to access the attributes of the object
-#corner1.identity, corner1.position
-corner2 = Corner("WGO", "UFL")
-#corner2.identity, corner1.position
+    def __repr__(self):
+        return f"Corner(identity={self.identity}, orientation={self.orientation})"
+
+
+
+class Edge: 
+    def __init__(self, identity, orientation = 0):
+        self.identity = identity
+        self.orientation = orientation
+
+    def __repr__(self):
+        return f"Edge(identity={self.identity}, orientation={self.orientation})"
+        
+
+class Cube:
+    def __init__(self):
+
+        self.corners = {
+            "URF": Corner("WRG"),
+            "ULF": Corner("WOG"),
+            "DRF": Corner("YRG"),
+            "DLF": Corner("YOG"),
+
+            "URB": Corner("WRB"),
+            "ULB": Corner("WOB"),
+            "DRB": Corner("YRB"),
+            "DLB": Corner("YOB")
+        }
+
+        # Edge slots
+        self.edges = {
+            "UF": Edge("WG"),
+            "UL": Edge("WO"),
+            "UB": Edge("WB"),
+            "UR": Edge("WR"),
+
+            "DF": Edge("YG"),
+            "DL": Edge("YO"),
+            "DB": Edge("YB"),
+            "DR": Edge("YR"),
+
+            "FR": Edge("GR"),
+            "FL": Edge("GO"),
+            "BL": Edge("BO"),
+            "BR": Edge("BR")
+        }
+
+print(Cube().corners["URF"])
