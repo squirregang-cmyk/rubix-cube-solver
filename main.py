28d1d803546bbae06d6d30a5e88da24a3fc7f76d
remi <squirregang@gmail.com>
2026-09-08 14:00:26 +0100

    created generic corner cycle and then created specific cycles using that function

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ class Edge:
 
     def __repr__(self):
         return f"Edge(identity={self.identity}, orientation={self.orientation})"
+
+
+class centre:
+    def __init__(self, identity):
+        self.identity = identity
+    
         
 
 class Cube:
@@ -50,41 +56,119 @@ class Cube:
             "BR": Edge("BR")
         }
 
+        self.centres = {
+            "U" : centre("W"),
+            "F" : centre("G"),
+            "R" : centre("R"),
+            "L" : centre("O"),
+            "B" : centre("B"),
+            "D" : centre("Y")
+        }
+        
+
 cube = Cube()
-print(cube.corners["URF"])
 
 # Define a function to cycle the corners on the R face
 
-def corner_cycle_R():
+def corner_cycle_no_twist(corner1, corner2, corner3, corner4):
     corners = cube.corners
-    temporary_corner = corners["URF"]
-    corners["URF"] = corners["DRF"]
-    corners["DRF"] = corners["DRB"]
-    corners["DRB"] = corners["URB"]
-    corners["URB"] = temporary_corner
+    temporary_corner = corners[corner1]
+    corners[corner1] = corners[corner2]
+    corners[corner2] = corners[corner3]
+    corners[corner3] = corners[corner4]
+    corners[corner4] = temporary_corner
 
-    corners["URF"].orientation = (corners["URF"].orientation + 1) % 3
-    corners["DRF"].orientation = (corners["DRF"].orientation + 1) % 3
-    corners["DRB"].orientation = (corners["DRB"].orientation + 1) % 3
-    corners["URB"].orientation = (corners["URB"].orientation + 1) % 3
+def corner_cycle_twist_clockwise(corner1, corner2, corner3, corner4):
+    corners = cube.corners
+    temporary_corner = corners[corner1]
+    corners[corner1] = corners[corner2]
+    corners[corner2] = corners[corner3]
+    corners[corner3] = corners[corner4]
+    corners[corner4] = temporary_corner
 
-#test to see if the corner cycle works
-#corner_cycle_R()
-#print(cube.corners["URF"])
-#corner_cycle_R()
-#print(cube.corners["URF"])
+    corners[corner1].orientation = (corners[corner1].orientation +1) % 3
+    corners[corner2].orientation = (corners[corner2].orientation +2) % 3
+    corners[corner3].orientation = (corners[corner3].orientation +1) % 3
+    corners[corner4].orientation = (corners[corner4].orientation +2) % 3
+
+def corner_cycle_twist_counter_clockwise(corner1, corner2, corner3, corner4):
+    corners = cube.corners
+    temporary_corner = corners[corner1]
+    corners[corner1] = corners[corner2]
+    corners[corner2] = corners[corner3]
+    corners[corner3] = corners[corner4]
+    corners[corner4] = temporary_corner
+
+    corners[corner1].orientation = (corners[corner1].orientation +2) % 3
+    corners[corner2].orientation = (corners[corner2].orientation +1) % 3
+    corners[corner3].orientation = (corners[corner3].orientation +2) % 3
+    corners[corner4].orientation = (corners[corner4].orientation +1) % 3
+
+
+def corner_cycle_U():
+    corner_cycle_no_twist("URF", "URB", "ULB", "ULF")
+
+
+
+def corner_cycle_U_prime():
+    corner_cycle_no_twist("URF", "ULF", "ULB", "URB")
+
+
+
+def corner_cycle_D():
+    corner_cycle_no_twist("DRF", "DRB", "DLB", "DLF")
+
+
+
+def corner_cycle_D_prime():
+    corner_cycle_no_twist("DRF", "DLF", "DLB", "DRB")
+
+
+
+def corner_cycle_R():
+    corner_cycle_twist_clockwise("URF", "DRF", "DRB", "URB")
+
+
+
+def corner_cycle_R_prime():
+    corner_cycle_twist_counter_clockwise("URF", "URB", "DRB", "DRF")
+
+
 
 def corner_cycle_L():
-    corners = cube.corners
-    temporary_corner = corners["ULF"]
-    corners["ULF"] = corners["ULB"]
-    corners["ULB"] = corners["DLB"]
-    corners["DLB"] = corners["DLF"]
-    corners["DLF"] = temporary_corner
+    corner_cycle_twist_clockwise("ULF", "ULB", "DLB", "DLF")
 
-    corners["ULF"].orientation = (corners["ULF"].orientation + 1) % 3
-    corners["ULB"].orientation = (corners["ULB"].orientation + 1) % 3
-    corners["DLB"].orientation = (corners["DLB"].orientation + 1) % 3
-    corners["ULF"].orientation = (corners["ULF"].orientation + 1) % 3
+
+
+def corner_cycle_L_prime():
+    corner_cycle_twist_counter_clockwise("ULF", "DLF", "DLB", "ULB")
+
+
+
+def corner_cycle_F():
+    corner_cycle_twist_clockwise("URF", "ULF", "DLF", "DRF")
+
+
+
+def corner_cycle_F_prime():
+    corner_cycle_twist_counter_clockwise("URF", "DRF", "DLF", "ULF")
+
+
+
+def corner_cylce_B():
+    corner_cycle_twist_clockwise("URB", "DRB", "DLB", "ULB")
+
+
+
+def corner_cylce_B_prime():
+    corner_cycle_twist_counter_clockwise("URB", "ULB", "DLB", "DRB")
+
+
+#tests to see if cycles work on specific slots   
+             
+for n in range(4): 
+    corner_cycle_U()
+    print(cube.corners["URF"])
+    n = n+1
 
     
