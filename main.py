88770967722f35d6f807f98d2f0911b70fd0b0f2
remi <squirregang@gmail.com>
2026-09-08 10:45:39 +0100

    creating an individual corners cycle for each move, will probably try to make a generic one where i can pass in the corners

--- main.py
+++ main.py
@@ -50,4 +50,41 @@ class Cube:
             "BR": Edge("BR")
         }
 
-print(Cube().corners["URF"])
+cube = Cube()
+print(cube.corners["URF"])
+
+# Define a function to cycle the corners on the R face
+
+def corner_cycle_R():
+    corners = cube.corners
+    temporary_corner = corners["URF"]
+    corners["URF"] = corners["DRF"]
+    corners["DRF"] = corners["DRB"]
+    corners["DRB"] = corners["URB"]
+    corners["URB"] = temporary_corner
+
+    corners["URF"].orientation = (corners["URF"].orientation + 1) % 3
+    corners["DRF"].orientation = (corners["DRF"].orientation + 1) % 3
+    corners["DRB"].orientation = (corners["DRB"].orientation + 1) % 3
+    corners["URB"].orientation = (corners["URB"].orientation + 1) % 3
+
+#test to see if the corner cycle works
+#corner_cycle_R()
+#print(cube.corners["URF"])
+#corner_cycle_R()
+#print(cube.corners["URF"])
+
+def corner_cycle_L():
+    corners = cube.corners
+    temporary_corner = corners["ULF"]
+    corners["ULF"] = corners["ULB"]
+    corners["ULB"] = corners["DLB"]
+    corners["DLB"] = corners["DLF"]
+    corners["DLF"] = temporary_corner
+
+    corners["ULF"].orientation = (corners["ULF"].orientation + 1) % 3
+    corners["ULB"].orientation = (corners["ULB"].orientation + 1) % 3
+    corners["DLB"].orientation = (corners["DLB"].orientation + 1) % 3
+    corners["ULF"].orientation = (corners["ULF"].orientation + 1) % 3
+
+    
